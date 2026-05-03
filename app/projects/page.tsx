40da8237a051a6e707e811ef06d183d82837b0b7
Daniel Muiruri <gathigidg26@gmail.com>
2026-05-03 14:15:28 +0300

    deleting un-used images

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
     url: 'https://daleondynamics.com/projects',
     images: [
       {
-        url: 'https://daleondynamics.com/_next/image?url=%2Fassets%2Flogo.png&w=48&q=75', // Use the same OG image you’ll create for the site
+        url: 'https://daleondynamics.com/_next/image?url=%2Fassets%2Flogo.png&w=48&q=75', 
         width: 1200,
         height: 630,
         alt: 'Daleon Dynamics Projects - Web Development Kenya',
